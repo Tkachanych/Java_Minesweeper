79957094638db54777de9e551fe7ae2252d1cdbd
Tkachanych <geletar@gmail.com>
2023-01-21 20:09:39 +0400

    add sizes and panel

--- JavaSweeper/src/JavaSweeper.java
+++ JavaSweeper/src/JavaSweeper.java
@@ -1,16 +1,28 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.WindowConstants;
+import javax.swing.plaf.DimensionUIResource;
+
 
 public class JavaSweeper extends JFrame {
+    JPanel panel;
     public static void main(String[] args) throws Exception {
         new JavaSweeper();
     }
 
     public JavaSweeper () {
+        
+        panel = new JPanel();
+        panel.setPreferredSize(new DimensionUIResource(500, 300));
+        add (panel);
+
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Java Sweeper");
-        setLocationRelativeTo(null);
         setVisible(true);
         setResizable(false);
+        
+        pack();
+        
+        setLocationRelativeTo(null);
     }
 }
